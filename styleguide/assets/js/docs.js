a833e36ac33f9e2f4bf6bb706cc483d1a9618a4d
Dennis Haulund Nielsen <dhni@nodes.dk>
2014-07-15 17:10:49 +0200

    Work work

--- styleguide/assets/js/docs.js
+++ styleguide/assets/js/docs.js
@@ -1,9 +1,6 @@
+console.log('docs');
 $( window ).bind( "create.xrayhtml", function( e ) {
     $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
 });
-
-$(document).ready(function() {
-    $(document).foundation();
-})
